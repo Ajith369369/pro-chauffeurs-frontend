0612b02754fb703c158de6c0a45d21b62207ae6b
Ajith K. S. <ajithks98086@gmail.com>
2024-07-30 04:35:52 +0530

    Add TextField container.

--- src/pages/HirerDetails.jsx
+++ src/pages/HirerDetails.jsx
@@ -1,3 +1,4 @@
+import TextField from "@mui/material/TextField";
 function HirerDetails() {
   return (
     <>
@@ -6,7 +7,16 @@ function HirerDetails() {
           <div className="col-sm-0 col-md-1 col-lg-1 col-xl-1 col-xxl-1"></div>
           <div className="col-sm-12 col-md-10 col-lg-10 col-xl-10 col-xxl-10 d-flex flex-column justify-content-center align-items-center">
             <div className="d-flex flex-column justify-content-center align-items-center">
-              <h4>Hirer Details</h4>
+              <h4 className="text-center">Hirer Details</h4>
+              <form action="">
+                <div className="form-group"></div>
+                <div className="form-group"></div>
+                <div className="form-group"></div>
+                <div className="form-group">
+                  <div></div>
+                  <div></div>
+                </div>
+              </form>
             </div>
           </div>
           <div className="col-sm-0 col-md-1 col-lg-1 col-xl-1 col-xxl-1"></div>
