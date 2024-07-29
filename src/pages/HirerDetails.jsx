3c28b63e4e95644078d92303d3c3341ee5522ca0
Ajith K. S. <ajithks98086@gmail.com>
2024-07-30 04:41:07 +0530

    Add state.

--- src/pages/HirerDetails.jsx
+++ src/pages/HirerDetails.jsx
@@ -1,5 +1,16 @@
 import TextField from "@mui/material/TextField";
+import { useState } from "react";
+
 function HirerDetails() {
+  // State Initialization
+  const [formState, setFormState] = useState({
+    passenger_name: "",
+    email: "",
+    mobile_number: "",
+    car_type: "",
+    car_number: "",
+  });
+
   return (
     <>
       <div className="container-fluid w-100">
@@ -9,7 +20,37 @@ function HirerDetails() {
             <div className="d-flex flex-column justify-content-center align-items-center">
               <h4 className="text-center">Hirer Details</h4>
               <form action="">
-                <div className="form-group"></div>
+                <div className="form-group">
+                  <TextField
+                    name="weight"
+                    value={formState.passenger_name || ""}
+                    className="w-100"
+                    id="outlined-basic"
+                    label="Weight (kg):"
+                    variant="outlined"
+                    sx={{
+                      // Root class for the input field
+                      "& .MuiOutlinedInput-root": {
+                        color: "#000000",
+                        fontFamily: "Arial",
+                        fontWeight: "bold",
+                        height: "60px",
+                        alignItems: "center",
+                        paddingLeft: "5px",
+                        // Class for the border around the input field
+                        "& .MuiOutlinedInput-notchedOutline": {
+                          borderColor: "#000000",
+                          borderWidth: "1px",
+                        },
+                      },
+                      // Class for the label of the input field
+                      "& .MuiInputLabel-outlined": {
+                        color: "black",
+                        fontSize: "16px",
+                      },
+                    }}
+                  />
+                </div>
                 <div className="form-group"></div>
                 <div className="form-group"></div>
                 <div className="form-group">
