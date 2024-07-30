7b30c6c03d6d1c1e807ce751ffd5214c190f4924
Ajith K. S. <ajithks98086@gmail.com>
2024-07-31 05:01:31 +0530

    Add API.

---
+++ src/services/pro_allApi.js
@@ -0,0 +1,22 @@
+import { commonAPI } from "./commonApi";
+import { serverUrl } from "./serverUrl";
+
+// API (from button) to upload (add) Booking Details (HirerDetails.jsx)
+export const addBookingDetailsOfAUserApi = async (reqBody) => {
+  return await commonAPI("POST", `${serverUrl}/allUsersBookingDetails`, reqBody);
+};
+
+// API to get all users' Booking Details
+export const getBookingDetailsOfAllUsersApi = async () => {
+  return await commonAPI("GET", `${serverUrl}/allUsersBookingDetails`, "");
+};
+
+// API to get a single user's Booking Details
+export const getBookingDetailsOfAUserApi = async (id) => {
+  return await commonAPI("GET", `${serverUrl}/allUsersBookingDetails/${id}`, "");
+};
+
+// API to delete a single user's Booking Details
+export const deleteBookingDetailsOfAUserApi = async (id) => {
+  return await commonAPI("DELETE", `${serverUrl}/allUsersBookingDetails/${id}`, {});
+};
