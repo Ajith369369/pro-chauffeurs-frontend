84c82bf04e13a76e4730e173c035f0dc56f328a1
Ajith K. S. <ajithks98086@gmail.com>
2024-08-01 05:13:12 +0530

    Change body's background color.

--- src/admin/Login.jsx
+++ src/admin/Login.jsx
@@ -1,11 +1,11 @@
-import React, { useState } from "react";
-import { Link, useNavigate } from "react-router-dom";
-import { ADMIN_USER } from "./constants";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faArrowLeft } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { useState } from "react";
+import Col from "react-bootstrap/Col";
+import Row from "react-bootstrap/Row";
+import { Link, useNavigate } from "react-router-dom";
 import loginImage from "../assets/lock-icon.jpg";
-import Row from 'react-bootstrap/Row';
-import Col from 'react-bootstrap/Col';
+import { ADMIN_USER } from "./constants";
 
 const Login = () => {
   const [username, setUsername] = useState("");
@@ -56,8 +56,12 @@ const Login = () => {
         style={{ width: "100%", height: "100vh" }}
       >
         <div className="container w-75">
-          <Link to={'/'} className="text-warning" style={{textDecoration:'none'}}>
-            <h4 className="text-warning">
+          <Link
+            to={"/"}
+            className="text-warning"
+            style={{ textDecoration: "none" }}
+          >
+            <h4 className="text-light">
               <FontAwesomeIcon icon={faArrowLeft} className="me-2" />
               Back Home
             </h4>
@@ -75,24 +79,9 @@ const Login = () => {
                 className="p-5 d-flex justify-content-center text-light"
               >
                 <form className="w-100">
-                  <h4 className="text-center text-light">
-                    <FontAwesomeIcon icon={faStackOverflow} className="fa-2x" />
-                    Project Fair
-                  </h4>
-                  {register ? (
-                    <h5 className="text-center">Sign Up to Your Account</h5>
-                  ) : (
-                    <h5 className="text-center">Sign In to Your Account</h5>
-                  )}
-                  {register && (
-                    <div className="mb-3">
-                      <input
-                        type="text"
-                        placeholder="Username"
-                        className="form-control rounded-0"
-                      />
-                    </div>
-                  )}
+                  <h4 className="text-center text-light">ProChauffeurs</h4>
+                  <h5 className="text-center">Sign In to Your Account</h5>
+
                   <div className="mb-3">
                     <input
                       type="text"
@@ -108,21 +97,17 @@ const Login = () => {
                     />
                   </div>
                   <div className="mb-3">
-                    {register ? (
-                      <div>
-                        <button className="btn btn-warning w-100 rounded-0">
+                    <div>
+                      <button className="btn btn-warning w-100 rounded-0">
+                        Login
+                      </button>
+                      <p>
+                        New User? Click Here to{" "}
+                        <Link to={"/register"} className="text-warning">
                           Register
-                        </button>
-                        <p>Already a User? Click Here to <Link to={'/login'} className="text-warning">Login</Link></p>
-                      </div>
-                    ) : (
-                      <div>
-                        <button className="btn btn-warning w-100 rounded-0">
-                          Login
-                        </button>
-                        <p>New User? Click Here to <Link to={'/register'}className="text-warning">Register</Link></p>
-                      </div>
-                    )}
+                        </Link>
+                      </p>
+                    </div>
                   </div>
                 </form>
               </Col>
@@ -131,7 +116,6 @@ const Login = () => {
         </div>
       </div>
     </>
-    
   );
 };
 
