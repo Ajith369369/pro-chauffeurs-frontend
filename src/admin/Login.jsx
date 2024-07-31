b28000a25b22e2bec0978a0c62c0d81550a7bec4
Ajith K. S. <ajithks98086@gmail.com>
2024-07-31 21:25:17 +0530

    Add Admin.

--- src/admin/Login.jsx
+++ src/admin/Login.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
-import { ADMIN_USER } from '../constants';
+import { ADMIN_USER } from './constants';
 
 const Login = () => {
   const [username, setUsername] = useState('');
