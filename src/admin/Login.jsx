d5c4b7e1f534499195d121039581dec06c004048
Ajith K. S. <ajithks98086@gmail.com>
2024-08-01 05:57:15 +0530

    Before replace.

--- src/admin/Login.jsx
+++ src/admin/Login.jsx
@@ -1,11 +1,13 @@
 import { faArrowLeft } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import TextField from "@mui/material/TextField";
 import { useState } from "react";
 import Col from "react-bootstrap/Col";
 import Row from "react-bootstrap/Row";
 import { Link, useNavigate } from "react-router-dom";
 import loginImage from "../assets/lock-icon.jpg";
 import { ADMIN_USER } from "./constants";
+import Button from "react-bootstrap/Button";
 
 const Login = () => {
   const [username, setUsername] = useState("");
@@ -79,28 +81,110 @@ const Login = () => {
                 className="p-5 d-flex justify-content-center text-light"
               >
                 <form className="w-100">
-                  <h3 className="text-center text-light">ProChauffeurs</h3>
-                  <h5 className="text-center">Sign In to Your Account</h5>
+                  <h3 className="text-center text-light mb-4">ProChauffeurs</h3>
+                  <h5 className="text-center my-3">Sign In to Your Account</h5>
 
-                  <div className="mb-3">
-                    <input
+                  <div className="mt-4 mb-3">
+                    {/* <input
                       type="text"
                       placeholder="Email ID"
                       className="form-control rounded-0"
+                    /> */}
+                    <TextField
+                      name="login_email"
+                      // value={loginState.email || ""}
+                      className="w-100"
+                      id="outlined-basic-1"
+                      label="EMAIL ID"
+                      variant="outlined"
+                      sx={{
+                        // Root class for the input field
+                        "& .MuiOutlinedInput-root": {
+                          color: "#000000",
+                          fontFamily: "Arial",
+                          fontWeight: "bold",
+                          height: "60px",
+                          alignItems: "center",
+                          paddingLeft: "5px",
+                          // Class for the border around the input field
+                          "& .MuiOutlinedInput-notchedOutline": {
+                            borderColor: "#000000",
+                            borderWidth: "1px",
+                          },
+                          // Change border color when focused
+                          "&.Mui-focused .MuiOutlinedInput-notchedOutline": {
+                            borderColor: "#ffffff",
+                          },
+                        },
+                        // Class for the label of the input field
+                        "& .MuiInputLabel-outlined": {
+                          color: "white",
+                          fontSize: "16px",
+                        },
+                        // Change label color when focused
+                        "& .MuiInputLabel-outlined.Mui-focused": {
+                          color: "white",
+                        },
+                      }}
                     />
                   </div>
                   <div className="mb-3">
-                    <input
+                    {/* <input
                       type="text"
                       placeholder="Password"
                       className="form-control rounded-0"
+                    /> */}
+                    <TextField
+                      name="login_pswd"
+                      // value={loginState.pswd || ""}
+                      className="w-100"
+                      id="outlined-basic-2"
+                      label="PASSWORD"
+                      variant="outlined"
+                      sx={{
+                        // Root class for the input field
+                        "& .MuiOutlinedInput-root": {
+                          color: "#000000",
+                          fontFamily: "Arial",
+                          fontWeight: "bold",
+                          height: "60px",
+                          alignItems: "center",
+                          paddingLeft: "5px",
+                          // Class for the border around the input field
+                          "& .MuiOutlinedInput-notchedOutline": {
+                            borderColor: "#000000",
+                            borderWidth: "1px",
+                          },
+                          // Change border color when focused
+                          "&.Mui-focused .MuiOutlinedInput-notchedOutline": {
+                            borderColor: "#ffffff",
+                          },
+                        },
+                        // Class for the label of the input field
+                        "& .MuiInputLabel-outlined": {
+                          color: "white",
+                          fontSize: "16px",
+                        },
+                        // Change label color when focused
+                        "& .MuiInputLabel-outlined.Mui-focused": {
+                          color: "white",
+                        },
+                      }}
                     />
                   </div>
                   <div className="mb-3">
                     <div>
-                      <button className="btn btn-warning w-100 rounded-0">
+                      {/* <button className="btn btn-warning w-100 rounded-0">
                         Login
-                      </button>
+                      </button> */}
+                      <Button
+                        variant="light"
+                        size="lg"
+                        className="mb-3 w-100 rounded-0"
+                        style={{ width: "150px" }}
+                      >
+                        Login
+                      </Button>
                       <p>
                         New User? Click Here to{" "}
                         <Link to={"/register"} className="text-warning">
--- src/admin/constants.js
+++ src/admin/constants.js
@@ -1,5 +1,5 @@
 export const ADMIN_USER = {
-  username: 'adminuser', // Set your admin username here
+  email_id: 'adminuser', // Set your admin username here
   password: 'adminpswd' // Set your admin password here 
   // It is not recommended to store passwords like this in production
 };
