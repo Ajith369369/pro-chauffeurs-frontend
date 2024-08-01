4dae993c78f437919d8b6bc5125484ec50309bb1
Bibinj09 <bibinj677@gmail.com>
2024-08-01 14:14:44 +0530

    home page added

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,21 @@
 import './App.css'
+import { BrowserRouter as Router,Route, Routes } from 'react-router-dom'
+import Home from './pages/Home'
+import './bootstrap.min.css'
 
 function App() {
 
   return (
     <>
-     <h1 className='bg-success'>Hello</h1>
+    <Router>
+    <Routes>
+      <Route path='/' element={<Home/>}/>
+     </Routes>
+    </Router>
+     
     </>
   )
 }
 
 export default App
+
