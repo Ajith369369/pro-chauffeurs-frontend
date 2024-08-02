6463486c5732c73f8c4504ccb66a74b6f98b9fdc
Ajith K. S. <ajithks98086@gmail.com>
2024-08-02 05:46:41 +0530

    Arranged code.

--- src/components/DriverCard.jsx
+++ src/components/DriverCard.jsx
@@ -6,7 +6,7 @@ import "../components/DriverCard.css";
 function DriverCard() {
   const navigate = useNavigate();
 
- /*  const handleBackClick = () => {
+  /*  const handleBackClick = () => {
     navigate("/driverlist");
   }; */
   const handleSelectDriverClick = () => {
--- src/pages/DriverList.jsx
+++ src/pages/DriverList.jsx
@@ -1,7 +1,7 @@
-import DriverCard from '../components/DriverCard'
+import { Button } from "react-bootstrap";
 import { useNavigate } from "react-router-dom";
-import   '../pages/DriverList.css'
-import { Button } from 'react-bootstrap'
+import DriverCard from "../components/DriverCard";
+import "../pages/DriverList.css";
 function DriverList() {
   const navigate = useNavigate();
   const handleDriverClick = () => {
@@ -12,38 +12,44 @@ function DriverList() {
   };
   return (
     <>
-    <div className='driverlist px-3 px-md-5 py-5'>
-      <div className="container">
-      <h1 className='text-light text-center  '>List Of Drivers</h1>
-      <div className="row ">
-        <div className="col-md-4 mt-5" onClick={handleDriverClick}>
-          <DriverCard/>
-        </div>
-        <div className="col-md-4 mt-5" onClick={handleDriverClick}>
-        <DriverCard/>
-        </div>
-        <div className="col-md-4 mt-5" onClick={handleDriverClick}>
-        <DriverCard/>
-        </div>
-        <div className="col-md-4 mt-5" onClick={handleDriverClick}>
-        <DriverCard/>
-        </div>
-        <div className="col-md-4 mt-5" onClick={handleDriverClick}>
-        <DriverCard/>
-        </div>
-        <div className="col-md-4 mt-5" onClick={handleDriverClick}>
-        <DriverCard/>
+      <div className="driverlist px-3 px-md-5 py-5">
+        <div className="container">
+          <h1 className="text-light text-center  ">List Of Drivers</h1>
+          <div className="row ">
+            <div className="col-md-4 mt-5" onClick={handleDriverClick}>
+              <DriverCard />
+            </div>
+            <div className="col-md-4 mt-5" onClick={handleDriverClick}>
+              <DriverCard />
+            </div>
+            <div className="col-md-4 mt-5" onClick={handleDriverClick}>
+              <DriverCard />
+            </div>
+            <div className="col-md-4 mt-5" onClick={handleDriverClick}>
+              <DriverCard />
+            </div>
+            <div className="col-md-4 mt-5" onClick={handleDriverClick}>
+              <DriverCard />
+            </div>
+            <div className="col-md-4 mt-5" onClick={handleDriverClick}>
+              <DriverCard />
+            </div>
+          </div>
+          <div className="mt-5 d-flex justify-content-center  ">
+            <Button
+              variant="light"
+              onClick={handleBackClick}
+              className="px-4 mb-5"
+              style={{ backgroundColor: "white", width: "150px" }}
+            >
+              Back
+            </Button>
+            {/* <Button variant="light" className='px-4' style={{ backgroundColor: "white" }}>Next</Button> */}
+          </div>
         </div>
       </div>
-      <div className='mt-5 d-flex justify-content-center  '>
-      <Button variant="light" onClick={handleBackClick} className='px-4 mb-5' style={{ backgroundColor: "white", width: "150px" }}>Back</Button>
-      {/* <Button variant="light" className='px-4' style={{ backgroundColor: "white" }}>Next</Button> */}
-      </div>
-      </div>
-    </div>
-
     </>
-  )
+  );
 }
 
-export default DriverList
+export default DriverList;
