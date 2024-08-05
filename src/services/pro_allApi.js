2ce0acb3428afac9699feb42d7954ebe413d44bf
Ajith K. S. <ajithks98086@gmail.com>
2024-08-05 18:27:04 +0530

    Edit services.

--- src/services/pro_allApi.js
+++ src/services/pro_allApi.js
@@ -52,8 +52,7 @@ export const getDetailsOfADriverApi = async (id) => {
   return await commonAPI("GET", `${serverUrl}/allDrivers/${id}`, "");
 };
 
-
-//  API to get all places (to caluclate amount based on distance between starting point and ending point)
+// API to get all places (to calculate amount based on distance between starting point and ending point)
 export const getPlacesApi = async () => {
   return await commonAPI("GET", `${serverUrl}/Allplaces`, "");
 };
