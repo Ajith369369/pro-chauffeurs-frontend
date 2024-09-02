5048636e5cc7ac765c298a1c971d6d7a5932ba61
Ajith K. S. <ajithks98086@gmail.com>
2024-09-02 13:10:57 +0530

    Change link.

--- src/services/pro_serverUrl.js
+++ src/services/pro_serverUrl.js
@@ -1,5 +1,5 @@
 // Base URL of the backend server
-// export const serverUrl = 'http://localhost:3000'
+export const serverUrl = 'http://localhost:3000'
 
 // Base URL of the backend server
-export const serverUrl = 'https://pro-chauffeurs-backend.onrender.com' 
+// export const serverUrl = 'https://pro-chauffeurs-backend.onrender.com' 
